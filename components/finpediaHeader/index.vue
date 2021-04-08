832df6f7bd9d0a9204f411fb134e5842a47d4b76
Andrian Soelistiyo <mac@192.168.1.4>
2021-04-09 00:47:33 +0700

    update header & modal

--- components/finpediaHeader/index.vue
+++ components/finpediaHeader/index.vue
@@ -2,20 +2,15 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-          />
+        <a class="navbar-item" href="/">
+          <img src="~/assets/img/logo/finpediaLogo.png" />
         </a>
-
         <a
-            role="button"
-            class="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -25,27 +20,24 @@
 
       <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-end">
-          <a class="navbar-item"> Pinjaman </a>
-
-          <a class="navbar-item"> Kartu Kredit </a>
-
-          <a class="navbar-item"> Simulasi KTA </a>
-
-          <a class="navbar-item"> Artikel </a>
-
-          <a class="navbar-item">
-            <figure class="image is-128x128 is-flex is-justify-content-center is-align-items-center">
-              <img class="is-rounded" src="https://via.placeholder.com/28x28" style="width: 30px; height: 30px;">
-              <span class="navbar-item"> Andrian</span>
+          <a href="/loan" class="navbar-item"> Pinjaman </a>
+          <a href="/loan" class="navbar-item"> Kartu Kredit </a>
+          <a href="#" class="navbar-item"> Simulasi KTA </a>
+          <a href="#" class="navbar-item"> Artikel </a>
+          <a href="#" class="navbar-item">
+            <figure
+              class="image is-flex is-justify-content-center is-align-items-center"
+            >
+              <img
+                class="is-rounded"
+                src="https://via.placeholder.com/28x28"
+                style="width: 24px; height: 24px"
+              />
+              <span class="navbar-item"> Andrian Soelistiyo </span>
             </figure>
           </a>
-
-          <a class="navbar-item">   <i class="fas fa-search "></i> </a>
-
-
-
+          <a href="#" class="navbar-item"> <i class="fas fa-search"></i> </a>
         </div>
-
       </div>
     </div>
   </nav>
@@ -53,6 +45,6 @@
 
 <script>
 export default {
-    name: 'finpediaHeader'
-}
+  name: "finpediaHeader",
+};
 </script>
--- components/popup/index.vue
+++ components/popup/index.vue
@@ -1,13 +1,12 @@
 <template>
-
   <div :class="['popup', activeClass]">
     <div class="popup__overlay" @click="$emit('closed')"></div>
-    
+
     <div class="popup__content-wrapper">
       <button class="popup__btn-close" @click="$emit('closed')">
         <span>x</span>
       </button>
-  
+
       <div class="popup__card">
         <div>
           <p>Testing</p>
@@ -15,19 +14,18 @@
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
 export default {
   name: "popup",
   props: {
-    active: Boolean
+    active: Boolean,
   },
   data() {
     return {
-      activeClass: ""
-    }
+      activeClass: "",
+    };
   },
   methods: {
     setClass() {
@@ -48,76 +46,76 @@ export default {
     // Will executed every changed on active state changed its value
     active: function (val, oldVal) {
       this.setClass();
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-  .popup {
-    display: none;
-    align-items: flex-end;
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    z-index: 999;
-  }
+.popup {
+  display: none;
+  align-items: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: 999;
+}
 
-  .d-flex {
-    display: flex;
-  }
+.d-flex {
+  display: flex;
+}
 
-  .popup__content-wrapper {
-    width: 100%;
-    max-height: 65%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    transform: translateY(100%);
-    transition: .25s transform ease;
-  }
-  .popup.active .popup__content-wrapper {
-    transform: translateY(0);
-  }
+.popup__content-wrapper {
+  width: 100%;
+  max-height: 65%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  transform: translateY(100%);
+  transition: 0.25s transform ease;
+}
+.popup.active .popup__content-wrapper {
+  transform: translateY(0);
+}
 
-  .popup__overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, .5);
-    opacity: 0;
-    transition: .25s opacity ease;
-  }
-  .popup.active .popup__overlay {
-    opacity: 1;
-  }
-  .popup__card {
-    position: relative;
-    max-height: 70%;
-    width: 100%;
-    margin-top: 10px;
-    padding: 1em;
-    background-color: #fff;
-    border-top-left-radius: 15px;
-    border-top-right-radius: 15px;
-    overflow-y: auto;
-  }
-  .popup__btn-close {
-    position: relative;
-    right: 15px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 35px;
-    width: 35px;
-    font-size: 22px;
-    border: none;
-    border-radius: 50%;
-    outline: none;
-  }
-  .popup__btn-close:active {
-    transform: scale(1.1) ;
-  }
+.popup__overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+  transition: 0.25s opacity ease;
+}
+.popup.active .popup__overlay {
+  opacity: 1;
+}
+.popup__card {
+  position: relative;
+  max-height: 70%;
+  width: 100%;
+  margin-top: 10px;
+  padding: 1em;
+  background-color: #fff;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  overflow-y: auto;
+}
+.popup__btn-close {
+  position: relative;
+  right: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 35px;
+  width: 35px;
+  font-size: 22px;
+  border: none;
+  border-radius: 50%;
+  outline: none;
+}
+.popup__btn-close:active {
+  transform: scale(1.1);
+}
 </style>
